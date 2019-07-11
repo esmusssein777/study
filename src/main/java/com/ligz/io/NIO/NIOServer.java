fd5edf3fe0d59e67b3c7526158e4a32be4da6ea3
esmusssein777 <380449395@qq.com>
2019-07-11 21:36:15 +0800

    netty学习

--- src/main/java/com/ligz/io/NIO/NIOServer.java
+++ src/main/java/com/ligz/io/NIO/NIOServer.java
@@ -2,85 +2,136 @@ package com.ligz.io.NIO;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.ServerSocket;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.Iterator;
-import java.util.Set;
 
 /**
- * author:ligz
+ * NIO服务端
+ * @author ligz
  */
 public class NIOServer {
-    public static void main(String[] args) throws IOException {
+    // 通道管理器
+    private Selector selector;
 
-        Selector selector = Selector.open();
+    /**
+     * 获得一个ServerSocket通道，并对该通道做一些初始化的工作
+     *
+     * @param port
+     *            绑定的端口号
+     * @throws IOException
+     */
+    public void initServer(int port) throws IOException {
+        // 获得一个ServerSocket通道
+        ServerSocketChannel serverChannel = ServerSocketChannel.open();
+        // 设置通道为非阻塞
+        serverChannel.configureBlocking(false);
+        // 将该通道对应的ServerSocket绑定到port端口
+        serverChannel.socket().bind(new InetSocketAddress(port));
+        // 获得一个通道管理器
+        this.selector = Selector.open();
+        // 将通道管理器和该通道绑定，并为该通道注册SelectionKey.OP_ACCEPT事件,注册该事件后，
+        // 当该事件到达时，selector.select()会返回，如果该事件没到达selector.select()会一直阻塞。
+        serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+    }
 
-        ServerSocketChannel ssChannel = ServerSocketChannel.open();
-        ssChannel.configureBlocking(false);
-        ssChannel.register(selector, SelectionKey.OP_ACCEPT);
-
-        ServerSocket serverSocket = ssChannel.socket();
-        InetSocketAddress address = new InetSocketAddress("127.0.0.1", 8888);
-        serverSocket.bind(address);
-
+    /**
+     * 采用轮询的方式监听selector上是否有需要处理的事件，如果有，则进行处理
+     *
+     * @throws IOException
+     */
+    public void listen() throws IOException {
+        System.out.println("服务端启动成功！");
+        // 轮询访问selector
         while (true) {
-
+            // 当注册的事件到达时，方法返回；否则,该方法会一直阻塞
             selector.select();
-            Set<SelectionKey> keys = selector.selectedKeys();
-            Iterator<SelectionKey> keyIterator = keys.iterator();
+            // 获得selector中选中的项的迭代器，选中的项为注册的事件
+            Iterator<?> ite = this.selector.selectedKeys().iterator();
+            while (ite.hasNext()) {
+                SelectionKey key = (SelectionKey) ite.next();
+                // 删除已选的key,以防重复处理
+                ite.remove();
 
-            while (keyIterator.hasNext()) {
-
-                SelectionKey key = keyIterator.next();
-
-                if (key.isAcceptable()) {
-
-                    ServerSocketChannel ssChannel1 = (ServerSocketChannel) key.channel();
-
-                    // 服务器会为每个新连接创建一个 SocketChannel
-                    SocketChannel sChannel = ssChannel1.accept();
-                    sChannel.configureBlocking(false);
-
-                    // 这个新连接主要用于从客户端读取数据
-                    sChannel.register(selector, SelectionKey.OP_READ);
-
-                } else if (key.isReadable()) {
-
-                    SocketChannel sChannel = (SocketChannel) key.channel();
-                    System.out.println(readDataFromSocketChannel(sChannel));
-                    sChannel.close();
-                }
-
-                keyIterator.remove();
+                handler(key);
             }
         }
     }
 
-    private static String readDataFromSocketChannel(SocketChannel sChannel) throws IOException {
+    /**
+     * 处理请求
+     *
+     * @param key
+     * @throws IOException
+     */
+    public void handler(SelectionKey key) throws IOException {
 
+        // 客户端请求连接事件
+        if (key.isAcceptable()) {
+            handlerAccept(key);
+            // 获得了可读的事件
+        } else if (key.isReadable()) {
+            handelerRead(key);
+        }
+    }
+
+    /**
+     * 处理连接请求
+     *
+     * @param key
+     * @throws IOException
+     */
+    public void handlerAccept(SelectionKey key) throws IOException {
+        ServerSocketChannel server = (ServerSocketChannel) key.channel();
+        // 获得和客户端连接的通道
+        SocketChannel channel = server.accept();
+        // 设置成非阻塞
+        channel.configureBlocking(false);
+
+        // 在这里可以给客户端发送信息哦
+        System.out.println("新的客户端连接");
+        // 在和客户端连接成功之后，为了可以接收到客户端的信息，需要给通道设置读的权限。
+        channel.register(this.selector, SelectionKey.OP_READ);
+    }
+
+    /**
+     * 处理读的事件
+     *
+     * @param key
+     * @throws IOException
+     */
+    public void handelerRead(SelectionKey key) throws IOException {
+        // 服务器可读取消息:得到事件发生的Socket通道
+        SocketChannel channel = (SocketChannel) key.channel();
+        // 创建读取的缓冲区
         ByteBuffer buffer = ByteBuffer.allocate(1024);
-        StringBuilder data = new StringBuilder();
+        int read = channel.read(buffer);
+        if(read > 0){
+            byte[] data = buffer.array();
+            String msg = new String(data).trim();
+            System.out.println("服务端收到信息：" + msg);
 
-        while (true) {
-
-            buffer.clear();
-            int n = sChannel.read(buffer);
-            if (n == -1) {
-                break;
-            }
-            buffer.flip();
-            int limit = buffer.limit();
-            char[] dst = new char[limit];
-            for (int i = 0; i < limit; i++) {
-                dst[i] = (char) buffer.get(i);
-            }
-            data.append(dst);
-            buffer.clear();
+            //回写数据
+            ByteBuffer outBuffer = ByteBuffer.wrap("好的".getBytes());
+            channel.write(outBuffer);// 将消息回送给客户端
+        }else{
+            System.out.println("客户端关闭");
+            key.cancel();
         }
-        return data.toString();
     }
+
+    /**
+     * 启动服务端测试
+     *
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        NIOServer server = new NIOServer();
+        server.initServer(8000);
+        server.listen();
+    }
+
 }
---
+++ src/main/java/com/ligz/io/netty/Server.java
@@ -0,0 +1,7 @@
+package com.ligz.io.netty;
+
+/**
+ * @author: ligz
+ */
+public class Server {
+}
